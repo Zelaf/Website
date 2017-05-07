57cdceaa6f7c58e8ced580ac753eb78ef883f50a
Zelaf Ary <zelaf@zelaf.eu>
2017-05-07 11:29:25 +0200

    Added mailgun and changed footer colour
    Added the mailgun thingys and changed the footer copyright background
    colour to match the footer background.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,45 @@
 <?php
+require 'PHPMailerAutoload.php';
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
-	
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$from = 'noreply@zelaf.eu';
+$to = 'zelaf@zelaf.eu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$reply_to = $email_address;
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$email_html_body = "<html>You have recieved an email from your websites contact form. <br><b>Name:</b> $name <br><b>Email:</b> $email_address <br><b>Message:</b> $message</html>";
+$mailgun_domain = 'noreply.zelaf.eu'; // should look like samples.mailgun.org or like mg.ardao.me (not necessarily the domain but the subdomain you configured mailgun on)
+$mailgun_smtp_password = '==PASSWORD==';
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.mailgun.org';
+$mail->SMTPAuth = true;
+$mail->Username = 'contact@'.$mailgun_domain; //If using default account this should be postmaster, not contact
+$mail->Password = $mailgun_smtp_password;
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+$mail->setFrom($from, $name);
+$mail->addAddress($to);
+$mail->addReplyTo($reply_to);
+$mail->Subject = $email_subject;
+$mail->Body = $email_html_body;
+$mail->AltBody = $email_body;
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+return true;
 ?>
